bfbac59e45ed55786316206d2bb914baba492e84
John Turkson <git@johnturkson.com>
2021-05-19 09:39:21 -0700

    Use Hilt SNAPSHOT builds due to compiler error in compose beta07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 30
     
     defaultConfig {
-        applicationId = "com.johnturkson.totpsync"
+        applicationId = "com.johnturkson.sync"
         minSdk = 26
         targetSdk = 30
         versionCode = 1
@@ -60,9 +60,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     
-    // Disabled due to a bug with compose beta07
-    implementation("com.google.dagger:hilt-android:2.35.1")
-    kapt("com.google.dagger:hilt-compiler:2.35.1")
+    implementation("com.google.dagger:hilt-android:HEAD-SNAPSHOT")
+    kapt("com.google.dagger:hilt-compiler:HEAD-SNAPSHOT")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
     
     implementation("androidx.room:room-runtime:2.3.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.android.tools.build:gradle:7.1.0-alpha01")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:HEAD-SNAPSHOT")
     }
 }
 
@@ -16,5 +16,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
