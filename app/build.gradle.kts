908b0a8761a452f41dd1e06e97828ee64396f13f
John Turkson <git@johnturkson.com>
2021-06-01 23:42:41 -0700

    Revert hilt compiler version after fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
+    
     implementation("androidx.core:core-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
@@ -59,8 +60,9 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     
-    implementation("com.google.dagger:hilt-android:HEAD-SNAPSHOT")
-    kapt("com.google.dagger:hilt-compiler:HEAD-SNAPSHOT")
+    implementation("com.google.dagger:hilt-android:2.36")
+    kapt("com.google.dagger:hilt-compiler:2.36")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha02")
     
     implementation("androidx.room:room-runtime:2.3.0")
     kapt("androidx.room:room-compiler:2.3.0")
@@ -77,6 +79,12 @@ dependencies {
     
     implementation("com.google.accompanist:accompanist-insets:0.10.0")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.10.0")
+    
+    implementation("androidx.camera:camera-core:1.1.0-alpha04")
+    implementation("androidx.camera:camera-camera2:1.1.0-alpha04")
+    implementation("androidx.camera:camera-lifecycle:1.1.0-alpha04")
+    implementation("androidx.camera:camera-view:1.0.0-alpha24")
+    implementation("com.google.mlkit:barcode-scanning:16.1.2")
     
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
     
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.android.tools.build:gradle:7.1.0-alpha01")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:HEAD-SNAPSHOT")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
     }
 }
 
