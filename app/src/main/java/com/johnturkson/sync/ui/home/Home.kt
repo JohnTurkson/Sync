5dfc3daef959a783c9c5d06acd5839178134cd79
JohnTurkson <git@johnturkson.com>
2022-01-09 02:17:52 -0800

    Cleanup imports

--- app/src/main/java/com/johnturkson/sync/ui/home/Home.kt
+++ app/src/main/java/com/johnturkson/sync/ui/home/Home.kt
@@ -1,7 +1,6 @@
 package com.johnturkson.sync.ui.home
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
