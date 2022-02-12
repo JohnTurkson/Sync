135bfe4326156717da6c7b56456b49bf985f5f33
JohnTurkson <git@johnturkson.com>
2022-02-11 20:38:10 -0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,29 +57,31 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
     
-    implementation("androidx.compose.ui:ui:1.2.0-alpha02")
-    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha02")
-    implementation("androidx.compose.runtime:runtime:1.2.0-alpha02")
-    implementation("androidx.compose.foundation:foundation:1.2.0-alpha02")
-    implementation("androidx.compose.material:material:1.2.0-alpha02")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha04")
+    implementation("androidx.compose.ui:ui:1.2.0-alpha03")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha03")
+    implementation("androidx.compose.runtime:runtime:1.2.0-alpha03")
+    implementation("androidx.compose.foundation:foundation:1.2.0-alpha03")
+    implementation("androidx.compose.material:material:1.2.0-alpha03")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha05")
     
-    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.4.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
     
     implementation("com.google.accompanist:accompanist-permissions:0.22.0-rc")
     
     implementation("com.google.dagger:hilt-android:2.40.5")
     kapt("com.google.dagger:hilt-compiler:2.40.5")
     
+    implementation("androidx.room:room-ktx:2.4.1")
     implementation("androidx.room:room-runtime:2.4.1")
     kapt("androidx.room:room-compiler:2.4.1")
-    implementation("androidx.room:room-ktx:2.4.1")
+    
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     
     implementation("androidx.camera:camera-core:1.1.0-beta01")
     implementation("androidx.camera:camera-camera2:1.1.0-beta01")
