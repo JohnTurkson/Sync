ecc724469571a4977ddce41bb40a2ec8333f3080
John Turkson <git@johnturkson.com>
2022-04-16 13:12:57 -0700

    Prepare for multi module setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.3.0-alpha05")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "7.3.0-alpha08" apply false
+    id("com.android.library") version "7.3.0-alpha08" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10" apply false
+    id("com.google.dagger.hilt.android") version "2.41" apply false
+    id("com.google.devtools.ksp") version "1.6.10-1.0.4" apply false
+    id("org.graalvm.buildtools.native") version "0.9.4" apply false
 }
